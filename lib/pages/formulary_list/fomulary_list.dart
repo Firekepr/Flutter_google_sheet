150233a514da913cb0745dd24bfe41611d2bf50e
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-08-31 18:57:39 -0300

    New formulary list screen and navigation functionality

---
+++ lib/pages/formulary_list/fomulary_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormularyList extends StatefulWidget {
+  const FormularyList({super.key});
+
+  @override
+  State<FormularyList> createState() => _FormularyListState();
+}
+
+class _FormularyListState extends State<FormularyList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_google_sheet/pages/formulary/formulary.dart';
+import 'package:project_google_sheet/pages/formulary_list/fomulary_list.dart';
 import 'package:project_google_sheet/pages/home/components/navigator_bar.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,6 +11,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final controller = PageController(initialPage: 0, keepPage: false);
   int _bottomNavIndex = 0;
 
   @override
@@ -19,11 +21,31 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Flutter google sheets'),
       ),
-      body: const Formulary(),
+      body: PageView(
+        controller: controller,
+        children: const [
+          Formulary(),
+          FormularyList(),
+        ],
+      ),
       bottomNavigationBar: NavigatorBar(
         bottomNavIndex: _bottomNavIndex,
-        onTap: (idx) => setState(() => _bottomNavIndex = idx),
+        onTap: _navigate,
       ),
     );
+  }
+
+  void _navigate(int idx) {
+    if (idx == controller.page) return;
+
+    if (idx > 0) {
+      controller.nextPage(duration: const Duration(milliseconds: 400), curve: Curves.ease);
+    } else {
+      controller.previousPage(duration: const Duration(milliseconds: 400), curve: Curves.ease);
+    }
+
+    _bottomNavIndex = idx;
+
+    setState(() {});
   }
 }
